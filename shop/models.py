9ea17ef2743eca673ca42cc5cfc2092d74d48c8d
Fjorena Alidini <fjorenaalidini0@gmail.com>
2026-05-26 01:03:30 +0200

    hapi 2 Të krijojmë strukturën e të dhënave: Category, Product, Review, Cart, CartItem.

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,117 @@
 from django.db import models
+from django.conf import settings
+from django.urls import reverse
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(unique=True, blank=True)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='categories/', blank=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:category_detail', kwargs={'slug': self.slug})
+
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name='products'
+    )
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(blank=True)
+    key_features = models.TextField(blank=True)
+    image = models.ImageField(upload_to='products/')
+    stock = models.PositiveIntegerField(default=10)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail', kwargs={'slug': self.slug})
+
+    @property
+    def average_rating(self):
+        reviews = self.reviews.all()
+        if not reviews:
+            return 0
+        return round(sum(r.rating for r in reviews) / reviews.count(), 1)
+
+    @property
+    def total_reviews(self):
+        return self.reviews.count()
+
+
+class Review(models.Model):
+    RATING_CHOICES = [(i, str(i)) for i in range(1, 6)]
+
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name='reviews'
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+    rating = models.IntegerField(choices=RATING_CHOICES)
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('product', 'user')
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.user} - {self.product} ({self.rating}★)'
+
+
+class Cart(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='cart'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Cart of {self.user}'
+
+    @property
+    def total_price(self):
+        return sum(item.subtotal for item in self.items.all())
+
+    @property
+    def total_items(self):
+        return sum(item.quantity for item in self.items.all())
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    class Meta:
+        unique_together = ('cart', 'product')
+
+    def __str__(self):
+        return f'{self.quantity}x {self.product.name}'
+
+    @property
+    def subtotal(self):
+        return self.quantity * self.product.price
